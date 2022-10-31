ad574155c70bcb9b7540b51d3aa8229e370bf0ef
Erick Romero <Erick_Romero@epam.com>
2022-10-31 10:26:19 -0500

    adding rest api

---
+++ src/main/java/com/okta/demo/oktasecuredemo/OktasecuredemoApplication.java
@@ -0,0 +1,22 @@
+package com.okta.demo.oktasecuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class OktasecuredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktasecuredemoApplication.class, args);
+	}
+
+	@GetMapping("api/users")
+	public List<String> getUsers() {
+		return List.of("Test", "Test1", "Test2");
+	}
+}
---
+++ src/test/java/com/okta/demo/oktasecuredemo/OktasecuredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okta.demo.oktasecuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktasecuredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
